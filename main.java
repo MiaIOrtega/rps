b4e7ccf85a8abf0de20c40256d0cebb7629188fa
Mia Ortega <miaortega2025@gmail.com>
2024-12-11 15:32:56 -0600

    Add files via upload

--- main.java
+++ main.java
@@ -12,70 +12,73 @@ public class main
             System.out.println("welcome to rock paper scissors");
             System.out.println("enter rock, paper, or scissors");
             String play = scanner.nextLine();
-            
+            int test = 0;
+            String a= "";
             if (true==play.contentEquals("rock")){
                 System.out.println("you: rock");
-                System.out.println(rock(cw,cd,cl));
+                if ((c())==1){
+                    System.out.println("computer: rock"); 
+                    cd=cd(cd);
+                    System.out.println("wins: "+cw+" loses: "+cl+" duals: "+cd);
+                }else if ((c())==2){
+                    System.out.println("computer: paper");
+                    cl=cl(cl);
+                    System.out.println("wins: "+cw+" loses: "+cl+" duals: "+cd);
+                }else if ((c())==3){
+                    System.out.println("computer: scissors");
+                    cw=cw(cw);
+                    System.out.println("wins: "+cw+" loses: "+cl+" duals: "+cd);
+                }
             }else if (true==play.contentEquals("paper")){
                 System.out.println("you: paper");
-                System.out.println(paper(cw,cd,cl));
+                if ((c())==1){
+                    System.out.println("computer: rock");
+                    cw=cw(cw);
+                    System.out.println("wins: "+cw+" loses: "+cl+" duals: "+cd);
+                }else if ((c())==2){
+                     System.out.println("computer: paper");
+                     cd=cd(cd);
+                    System.out.println("wins: "+cw+" loses: "+cl+" duals: "+cd);
+                }else if ((c())==3){
+                     System.out.println("computer: scissors");
+                    cl=cl(cl);
+                    System.out.println("wins: "+cw+" loses: "+cl+" duals: "+cd);
+                }
             }else if (true==play.contentEquals("scissors")){
                 System.out.println("you: scissors");
-                System.out.println(scissors(cw,cd,cl));
+                if ((c())==1){
+                    System.out.println("computer: rock");
+                    cl=cl(cl);
+                    System.out.println("wins: "+cw+" loses: "+cl+" duals: "+cd);
+                }else if ((c())==2){
+                    System.out.println("computer: paper");
+                    cw=cw(cw);
+                    System.out.println("wins: "+cw+" loses: "+cl+" duals: "+cd);
+                }else if ((c())==3){
+                     System.out.println("computer: scissors");
+                     cd=cd(cd);
+                    System.out.println("wins: "+cw+" loses: "+cl+" duals: "+cd);
+                }
             }else{
-            System.out.println("did not enter rock, paper, or scissors");
+                System.out.println("you did not enter rock, paper, or scissors");
             }
            
             
-            System.out.println("do you want to play again ");
+            System.out.println("do you want to play again (yes/no)");
             String again = scanner.nextLine();
             if (true==again.contentEquals("yes")){
-            playing=true;
+                playing=true;
             }else if (true==again.contentEquals("no")){
-            playing=false;
+                playing=false;
             }
         }
          
             System.out.println("thanks for playing");
     }
     
-    public static String rock(int w, int d, int l){
-            String p= set(ran());
-            String a="";
-            if (true==p.contentEquals("rock")){
-                a="computer: rock \ndual\nwins: "+w+" loses: "+l+" duals: "+cd(d);   
-            }else if (true==p.contentEquals("paper")){
-                a="computer: paper \nyou lose\nwins: "+w+" loses: "+cl(l)+" duals: "+d; 
-         }else if (true==p.contentEquals("scissors")){
-                a="computer: scissors \nyou win\nwins: "+cw(w)+" loses: "+l+" duals: "+d;  
-            }
-            return a;
-    }
-    
-    public static String paper(int w, int d, int l){
-     String p= set(ran());
-            String a="";
-            if (true==p.contentEquals("rock")){
-                a="computer: rock \nyou win\nwins: "+cw(w)+" loses: "+l+" duals: "+ d;
-            }else if (true==p.contentEquals("paper")){
-                a="computer: paper \ndual\nwins: "+w+" loses: "+l+" duals: "+cd(d);  
-         }else if (true==p.contentEquals("scissors")){
-                a="computer: scissors \nyou lose\nwins: "+w+" loses: "+cl(l)+" duals: "+d; 
-            }
-            return a;
-    }
-    
-    public static String scissors(int w, int d, int l){
-     String p= set(ran());
-            String a="";
-            if (true==p.contentEquals("rock")){
-                a="computer: rock \nyou lose\nwins: "+w+" loses: "+cl(l)+" duals: "+d;  
-            }else if (true==p.contentEquals("paper")){
-                a="computer: paper \nyou win\nwins: "+cw(w)+" loses: "+l+" duals: "+d;  
-         }else if (true==p.contentEquals("scissors")){
-                a="computer: scissors \ndual\nwins: "+w+" loses: "+l+" duals: "+cd(d);  
-            }
-            return a;
+    public static int c(){
+            int p= ran();
+            return p;
     }
     
      public static int ran(){
@@ -84,30 +87,20 @@ public class main
         int num = (int)(Math.random() * (max - min + 1)) + min; 
         return num;
     }
-     public static String set(int num){
-        String p="";
-         if (num==1){
-        p="rock";
-        }else if (num==2){
-        p = "paper";
-        }else if (num==3){
-        p="scissors";
-        }
-        return p;
-    }
-    
     
     public static int cw (int c){
         c++; 
         return c;
     }
+    
     public static int cl (int c){
         c++; 
         return c;
     }
+    
     public static int cd (int c){
         c++;
         return c;
     }
-    //add count funtion
+    
 }
